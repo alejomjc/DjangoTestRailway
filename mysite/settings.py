ee1184c8df9fd2dd75efe5ab35cfe47f0306138e
Alejandro <alejomjc2011@gmail.com>
2022-11-20 12:13:09 -0500

    CSRF_TRUSTED_ORIGINS

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 DEBUG = True
 
 ALLOWED_HOSTS = ["django-server-production-ed7c.up.railway.app", '127.0.0.1', 'localhost']
+CSRF_TRUSTED_ORIGINS = ['https://django-server-production-ed7c.up.railway.app/']
 
 # Application definition
 
